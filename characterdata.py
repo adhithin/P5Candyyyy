8244f0c90a5df8dc081e0460e2b8c29baa22dfb2
Arul Salaniwal <arul.salaniwal@gmail.com>
2020-12-17 15:59:57 -0800

    Update characterdata.py

--- characterdata.py
+++ characterdata.py
@@ -11,7 +11,7 @@ donkey_kong = {
     "name": "Donkey Kong",
     "game": "Mario Kart",
     "backstory": "Donky Kong is a powerful ape who loves eating bananas more than anything. "
-                 "He is seen as Mario's enemy"
+                 "He is Mario's enemy"
 }
 
 print (donkey_kong)
@@ -20,7 +20,7 @@ mario = {
     "name": "Mario",
     "game": "Mario Bros",
     "backstory": "Mario is a plumber who is also the protector of the Mushroom Kingdom."
-                 "He is seen as a Hero."
+                 "He is everyone's friend"
 }
 
 print (mario)
@@ -29,7 +29,7 @@ luigi = {
     "name": "Luigi",
     "game": "Mario Kart",
     "backstory": "Luigi is Mario's brother and a plumber"
-                 "He is seen as Mario's partner"
+                 "He is Mario's partner on missions"
 }
 
 print(luigi)
@@ -38,7 +38,7 @@ sonic = {
     "name": "Sonic",
     "game": "Sonic the Hedgehog",
     "backstory": "Sonic is a blue hedgehog with supersonic speed and the ability to roll into a ball and face his enemies"
-                 "He is seen as a Hero"
+                 "He is perceived as a hero"
 }
 
 print(sonic)

065d870af0e2174716484c6942985cfb26f1e8d2
AkhileshLG <55467771+AkhileshLG@users.noreply.github.com>
2020-12-18 13:24:34 -0800

    I added the room1 and room2 routes

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route('/room1')
+def room1():
+    return render_template("room1.html")
+
+@app.route('/room2')
+def room2():
+    return render_template("room2.html")
 
 if __name__ == "__main__":
     #runs the application on the repl development server

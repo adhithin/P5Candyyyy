d8a29d008071c263f6830566d4ca85f03b3d76b5
adhithin <adhithi.nmurthy07@gmail.com>
2020-12-20 22:50:25 -0800

    revised home page

--- templates/playerdata.py
+++ templates/playerdata.py
@@ -159,3 +159,23 @@ def phones():
         user_dict['phone_numbers'] = phone.phone_number
         records.append(user_dict)
     return render_template("pythondb/index.html", table=records, menu=menus)
+
+rom __init__ import db
+
+# declare the users database model
+class Users(db.Model):
+    UserID = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), unique=True, nullable=False)
+
+
+# Declare emails database model
+class Emails(db.Model):
+    UserID = db.Column(db.Integer, primary_key=True)
+    email_address = db.Column(db.String(255), unique=True, nullable=False)
+
+
+# declare phone numbers database model
+class PhoneNumbers(db.Model):
+    UserID = db.Column(db.Integer, primary_key=True)
+    phone_number = db.Column(db.String(255), unique=True, nullable=False)

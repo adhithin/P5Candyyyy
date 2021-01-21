25e9e0a3d14a37bb70d1d51ec4ef2d0b4a00345a
AkhileshLG <55467771+AkhileshLG@users.noreply.github.com>
2021-01-20 21:09:44 -0800

    puzzle 1 user_interface

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,7 @@ from views import app
 
 if __name__ == "__main__":
     #runs the application on the repl development server
-    app.run(debug=True, port='80', host='192.168.86.51')
+    app.run(debug=True, port='80', host='127.0.0.1')
+
     #192.168.86.51
     #127.0.0.1

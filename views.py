68d10f0e6dde1104bd1522ce15d38286fcdb6d4a
adhithin <adhithi.nmurthy07@gmail.com>
2021-01-21 11:50:48 -0800

    updated view.py and login

--- views.py
+++ views.py
@@ -46,7 +46,7 @@ def login():
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
-            return redirect(url_for('home'))
+            return redirect(url_for('room1'))
     return render_template('login.html', error=error)
 
 @app.route('/register')

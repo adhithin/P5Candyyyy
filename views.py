7c3660aaea9c2399d9ca08b4a675d95cffa33b42
Pedro Medeiros <72947432+PedroBMedeiros@users.noreply.github.com>
2021-02-16 13:04:33 -0800

    Update views.py

--- views.py
+++ views.py
@@ -3,20 +3,20 @@ import os
 
 from flask import Flask, render_template, redirect, url_for
 from flask import request
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
-# from __init__ import app
-# from models.lessons import menus, TITLE, PROJECTS, select_2_proj, lessons_dict
-# import requests
+from __init__ import app
+from models.lessons import menus, TITLE, PROJECTS, select_2_proj, lessons_dict
+import requests
 
-# from flask import Flask, render_template, request, url_for
-# from werkzeug.utils import redirect
+from flask import Flask, render_template, request, url_for
+from werkzeug.utils import redirect
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 class Score(db.Model):
     __tablename__ = 'scores'

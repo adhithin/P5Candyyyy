13aa533d175742728a8ba600c4a0703fa90b64fa
Pedro Medeiros <72947432+PedroBMedeiros@users.noreply.github.com>
2021-02-16 13:05:20 -0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,6 @@ from views import app
 
 if __name__ == "__main__":
     #runs the application on the repl development server
-    app.run(debug=True, port='80', host='192.168.86.51')
+    views.app.run(debug=True, port='8080', host='192.168.86.51')
     #192.168.86.51
     #127.0.0.1

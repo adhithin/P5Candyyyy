7e142dfe1f0c0702e606acc73138c899ca0cdb4c
adhithin <adhithi.nmurthy07@gmail.com>
2021-02-18 14:07:01 -0800

    a few organizations

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,6 @@ from app import app
 
 if __name__ == "__main__":
     #runs the application on the repl development server
-    app.run(debug=True, port='80', host='192.168.86.51')
+    app.run(debug=True, port='8090', host='127.0.0.1')
     #192.168.86.51
     #127.0.0.1

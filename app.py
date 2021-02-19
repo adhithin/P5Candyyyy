e4c7293dcf7a6716dc3775a88546b3bb4a92736f
adhithin <adhithi.nmurthy07@gmail.com>
2021-02-19 15:00:17 -0800

    worked on game 3 - need to create a table for leaderboard

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def game3():
 
 	if request.method == 'POST':
 		name = request.form['name']
-		score = int(request.form['score'])
+		score = request.form['score']
 		game = request.form['game']
 		#the code below confirmed I had the proper data. Now to add it to the db.
 		#print(Score(name, score, game))
